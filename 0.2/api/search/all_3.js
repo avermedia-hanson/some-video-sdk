7684b2644cde22c8b065b9946a3c09f1b582e8bd
Hanson Wang <Hanson.Wang@avermedia.com>
2025-02-11 10:03:03 +0800

    Deployed cffb927 to 0.2 with MkDocs 1.6.1 and mike 2.1.3

---
+++ 0.2/api/search/all_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['sdk_20api_20reference_0',['Some Video SDK API Reference',['../index.html',1,'']]],
+  ['some_20video_20sdk_20api_20reference_1',['Some Video SDK API Reference',['../index.html',1,'']]]
+];
